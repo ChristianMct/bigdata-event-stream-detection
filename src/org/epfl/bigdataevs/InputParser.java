bc1e04e528a77a787d22b17b5ea775655d9704e0
Christian <christian.mouchet@epfl.ch>
2015-03-25 10:11:30 +0100

    [Added] Classe vide pour definition d'interface

---
+++ src/org/epfl/bigdataevs/InputParser.java
@@ -0,0 +1,10 @@
+package org.epfl.bigdataevs;
+
+/**
+*	Team: Matias et Christian
+*
+*	InputParser: Parse les données lues depuis HDFS, clean, et calcule le background model pour le EM algorithm
+**/
+public class InputParser {
+
+}
