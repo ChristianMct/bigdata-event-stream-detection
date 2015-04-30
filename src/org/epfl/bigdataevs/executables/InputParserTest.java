14f95dd6afc67ea4345e191b9c657b99c9bb60e0
Kognefaure <maticerc@hotmail.com>
2015-04-30 11:57:49 +0200

    InputParserTest automatically fragments a time frame in time periods of one month length. Use the constants NUM_PARTITIONS and YEAR_START to set your time frame

--- src/org/epfl/bigdataevs/executables/InputParserTest.java
+++ src/org/epfl/bigdataevs/executables/InputParserTest.java
@@ -11,12 +11,18 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.xml.stream.XMLStreamException;
 
 public class InputParserTest {
+  
+  /* Number of time periods in our time frame. A time period is one month long*/
+  public static final int NUM_PARTITIONS = 60;
+  public static final long YEAR_START = 1939;
 
   public static void main(String[] args) throws NumberFormatException, XMLStreamException, ParseException, IOException {
     
@@ -27,11 +33,21 @@ public class InputParserTest {
     JavaSparkContext ctx = new JavaSparkContext(sparkConf);
     
     DateFormat format = new SimpleDateFormat("dd/MM/yyyy-HH");
-    
-    
-    List<TimePeriod> timePeriods = new ArrayList<TimePeriod>(); 
-    timePeriods.add(new TimePeriod(format.parse("1/1/1939-11"), format.parse("1/1/1940-13")));
-    
+    Calendar c = Calendar.getInstance(); 
+    List<TimePeriod> timePeriods = new ArrayList<TimePeriod>();
+    /*Build our time frame by iteratively adding time periods of one month length*/
+    Date start = format.parse("2/1/" + YEAR_START + "-11");
+    Date end = format.parse("1/2/" + YEAR_START + "-13");
+    for (int i=0; i<NUM_PARTITIONS; i++) {
+      timePeriods.add(new TimePeriod(start, end));
+      c.setTime(start); 
+      c.add(Calendar.MONTH, 1);
+      start = c.getTime();
+      c.setTime(end); 
+      c.add(Calendar.MONTH, 1);
+      end = c.getTime();
+    }
+
     //System.out.println(timePeriods.get(0).includeDates(format.parse("1/1/1939-12")));
     
     List<String> inputPaths = new LinkedList<String>();
